8fdf51e27359f40abe94752a745ecba5753d8920
BenjamimOliveira <bmoliveira03@gmail.com>
2019-01-21 15:02:17 +0000

    Add pause and stop buttons on each music

--- player/player.js
+++ player/player.js
@@ -1,5 +1,15 @@
-function play(){
 var sound = new Howl({
-    src: ['music/death.mp3']
-  }).play();
+  src: ['music/death.mp3']
+})
+
+function play(){
+  sound.play();
+}
+
+function pause(){
+  sound.pause();
+}
+
+function stop(){
+  sound.stop();
 }
