5117757abfd9ef7308e0e6c326b9c272644fdde7
BenjamimOliveira <bmoliveira03@gmail.com>
2019-01-21 15:46:55 +0000

    Add volume controller and music progression controller

--- player/player.js
+++ player/player.js
@@ -1,3 +1,8 @@
+//Volume
+var volume = document.getElementById("volume");
+var progresso = document.getElementById("progresso");
+
+// ----------------------------------
 var sound = new Howl({
   src: ['music/death.mp3']
 })
@@ -13,3 +18,15 @@ function pause(){
 function stop(){
   sound.stop();
 }
+
+// Volume controll
+volume.oninput = function() {  
+  sound.volume((this.value)/100);
+} 
+
+// Music progression controll
+progresso.oninput = function() {  
+  if (sound.playing()){
+    sound.seek(sound.duration() * (this.value/100));
+  }
+} 
